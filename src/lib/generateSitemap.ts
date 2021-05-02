c6687f4a310caba0abef996eb2b1f92fe9594fa8
Hiro-mackay <johnmackay150@gmail.com>
2021-05-02 11:55:19 +0900

    ignore 404 & [slug]page from sitemap

--- src/lib/generateSitemap.ts
+++ src/lib/generateSitemap.ts
@@ -2,7 +2,7 @@ import globby from 'globby';
 import fs from 'fs';
 
 async function generateSiteMap() {
-  const pages = await globby(['.next/server/pages/**/*.html', '.next/serverless/pages/**/*.html']);
+  const pages = await globby(['.next/server/pages/**/*.html', '.next/serverless/pages/**/*.html', '!.next/serverless/pages/**/[*', '!.next/serverless/pages/404']);
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">   
