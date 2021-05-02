46a0eecaec762027819cbf0b9708629ad7f24218
Hiro-mackay <johnmackay150@gmail.com>
2021-05-02 15:55:52 +0900

    add google analytics script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,22 @@
 import '../styles/tailwind.compiled.css';
 import 'katex/dist/katex.css';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+import { useEffect } from 'react';
 
-export default ({ Component, pageProps }) => <Component {...pageProps} />;
+export default ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (!gtag.GA_TRACKING_ID) return;
+
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+  return <Component {...pageProps} />;
+};
