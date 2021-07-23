76a1b285bc2312ae6dee1e53f121b2dbdb349c1c
Hiro-mackay <johnmackay150@gmail.com>
2021-07-23 10:02:01 +0900

    script change double quotation

--- src/components/Head/index.tsx
+++ src/components/Head/index.tsx
@@ -21,16 +21,18 @@ export const Head = ({ titlePre, description = '' }) => {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="/images/og-image.png" />
       <script>
-        {`(function(d) {
-                var config = {kitId: 'azl7goz',scriptTimeout: 3000,async: true},
-                h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-            })(document)`}
+        {`(function (d) {
+          var config = {
+            kitId: "azl7goz", scriptTimeout: 3000, async: true
+          },
+          h = d.documentElement, t = setTimeout(function () { h.className = h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive"; }, config.scriptTimeout), tk = d.createElement("script"), f = false, s = d.getElementsByTagName("script")[0], a; h.className += " wf-loading"; tk.src = 'https://use.typekit.net/' + config.kitId + '.js'; tk.async = true; tk.onload = tk.onreadystatechange = function () { a = this.readyState; if (f || a && a != "complete" && a != "loaded") return; f = true; clearTimeout(t); try { Typekit.load(config) } catch (e) { } }; s.parentNode.insertBefore(tk, s)
+        })(document)`}
       </script>
       
       {GA_TRACKING_ID && (
         <>
           <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}></script>
-          <script>{`window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GA_TRACKING_ID}');`}</script>
+          <script>{`window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag("js", new Date()); gtag("config", "${GA_TRACKING_ID}");`}</script>
         </>
       )}
     </Heads>
