ab9bd0de64e3eab3f4d17071a5483864ef6183d0
Hiro-mackay <johnmackay150@gmail.com>
2021-10-16 13:31:28 +0900

    add News archive page

--- src/pages/news/index.tsx
+++ src/pages/news/index.tsx
@@ -1,3 +1,47 @@
-export const Page = () => {
-  return <div></div>;
+import Link from 'next/link';
+import { Head } from '../../components/Head';
+import { ArticleListItem } from '../../components/ArticleListItem';
+import blogStyles from '../../styles/blog.module.css';
+import { postIsPublished } from '../../lib/blog-helpers';
+import { MainVisual } from '../../components/MainVisual';
+import { Corporate } from '../../layouts/corporate';
+import { databaseId, NewsProperties, NewsResults } from '../../lib/notion/types';
+import { GetStaticPropsResult } from 'next';
+import { getDatabase } from '../../lib/notion';
+
+type PageProps = { posts: NewsResults };
+
+export const getStaticProps = async (): Promise<GetStaticPropsResult<PageProps>> => {
+  const posts = await getDatabase<NewsProperties>(databaseId);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 1,
+  };
 };
+
+const Page = ({ posts }: PageProps) => {
+  return (
+    <Corporate>
+      <Head titlePre="News" />
+      <MainVisual title="NEWS" />
+      <section className="container mx-auto py-24 px-8 md:px-44">
+        <div className="grid grid-cols-1 divide-y-2 divide-gray-300">
+          {posts.length === 0 && <p className={blogStyles.noPosts}>ニュースがまだありません</p>}
+          {posts.map((post) => {
+            return (
+              post && (
+                <div className="px-3 py-8 border-b border-gray-300" key={post.id}>
+                  <ArticleListItem {...post}></ArticleListItem>
+                </div>
+              )
+            );
+          })}
+        </div>
+      </section>
+    </Corporate>
+  );
+};
+export default Page;
